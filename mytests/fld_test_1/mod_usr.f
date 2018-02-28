08d87d155b283e34436eb4037f50478b2f7a35e5
nmoensmail@gmail.com <nicolasm@benetasch.ster.kuleuven.be>
2018-02-28 13:55:45 +0100

    radiation testcase added

---
+++ mytests/fld_test_1/mod_usr.f
@@ -0,0 +1,175 @@
+!> This is a template for a new user problem
+module mod_usr
+
+  ! Include a physics module
+  use mod_hd
+  use mod_fld
+
+  implicit none
+
+contains
+
+  !> This routine should set user methods, and activate the physics module
+  subroutine usr_init()
+    use mod_global_parameters
+    use mod_usr_methods
+
+    call set_coordinate_system("Cartesian_2D")
+
+    ! Initialize units
+    usr_set_parameters => initglobaldata_usr
+
+    ! A routine for initial conditions is always required
+    usr_init_one_grid => initial_conditions
+
+    ! Keep the radiative energy constant with internal bound
+    usr_internal_bc => constant_r_e
+
+    ! Output routines
+    usr_aux_output    => specialvar_output
+    usr_add_aux_names => specialvarnames_output
+
+    ! Active the physics module
+    call hd_activate()
+
+    print*, 'unit_time', unit_time
+    print*, 'unit_temperature', unit_temperature
+    print*, 'unit_length', unit_length
+    print*, 'unit_density', unit_density
+    !print*, 'unit_energy', unit_energy
+    print*, '================================================================'
+
+  end subroutine usr_init
+
+!==========================================================================================
+
+subroutine initglobaldata_usr
+  use mod_global_parameters
+
+  !Fix dimensionless stuff here
+  unit_length        = 1.d0                                         ! cm
+  unit_density       = 1.d0 !g cm-3,cm-3
+  unit_temperature   = 1.d0                                         ! K
+
+end subroutine initglobaldata_usr
+
+!==========================================================================================
+
+  !> A routine for specifying initial conditions
+  subroutine initial_conditions(ixGmin1,ixGmin2,ixGmax1,ixGmax2, ixmin1,ixmin2,&
+     ixmax1,ixmax2, w, x)
+    use mod_global_parameters
+    use mod_constants
+    use mod_hd_phys, only: hd_get_pthermal
+
+    integer, intent(in)             :: ixGmin1,ixGmin2,ixGmax1,ixGmax2, ixmin1,&
+       ixmin2,ixmax1,ixmax2
+    double precision, intent(in)    :: x(ixGmin1:ixGmax1,ixGmin2:ixGmax2,&
+        ndim)
+    double precision, intent(inout) :: w(ixGmin1:ixGmax1,ixGmin2:ixGmax2, nw)
+
+    double precision :: temperature(ixGmin1:ixGmax1,ixGmin2:ixGmax2)
+    double precision :: int_energy(ixGmin1:ixGmax1,ixGmin2:ixGmax2)
+
+    integer :: i
+
+    ! Set initial values for w
+    w(ixGmin1:ixGmax1,ixGmin2:ixGmax2, rho_) = 1d-7
+    w(ixGmin1:ixGmax1,ixGmin2:ixGmax2, mom(1)) = zero
+    w(ixGmin1:ixGmax1,ixGmin2:ixGmax2, mom(2)) = zero
+    w(ixGmin1:ixGmax1,ixGmin2:ixGmax2, e_) = 1d10
+    w(ixGmin1:ixGmax1,ixGmin2:ixGmax2,r_e) = 1d12
+
+  end subroutine initial_conditions
+
+!==========================================================================================
+
+  ! Extra routines can be placed here
+  ! ...
+
+!==========================================================================================
+
+!> internal boundary, user defined
+  !
+  !> This subroutine can be used to artificially overwrite ALL conservative
+  !> variables in a user-selected region of the mesh, and thereby act as
+  !> an internal boundary region. It is called just before external (ghost cell)
+  !> boundary regions will be set by the BC selection. Here, you could e.g.
+  !> want to introduce an extra variable (nwextra, to be distinguished from nwaux)
+  !> which can be used to identify the internal boundary region location.
+  !> Its effect should always be local as it acts on the mesh.
+
+  subroutine constant_r_e(level,qt,ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,&
+     ixOmin2,ixOmax1,ixOmax2,w,x)
+    use mod_global_parameters
+    integer, intent(in)             :: ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,&
+       ixOmin2,ixOmax1,ixOmax2,level
+    double precision, intent(in)    :: qt
+    double precision, intent(inout) :: w(ixImin1:ixImax1,ixImin2:ixImax2,1:nw)
+    double precision, intent(in)    :: x(ixImin1:ixImax1,ixImin2:ixImax2,&
+       1:ndim)
+
+    w(ixImin1:ixImax1,ixImin2:ixImax2,r_e) = 1d12
+
+    print*, 'WHYSIS THIS NOT WORKING', w(50,50,e_)
+
+  end subroutine constant_r_e
+
+!==========================================================================================
+
+subroutine specialvar_output(ixImin1,ixImin2,ixImax1,ixImax2,ixOmin1,ixOmin2,&
+   ixOmax1,ixOmax2,w,x,normconv)
+! this subroutine can be used in convert, to add auxiliary variables to the
+! converted output file, for further analysis using tecplot, paraview, ....
+! these auxiliary values need to be stored in the nw+1:nw+nwauxio slots
+!
+! the array normconv can be filled in the (nw+1:nw+nwauxio) range with
+! corresponding normalization values (default value 1)
+  use mod_global_parameters
+  use mod_physics
+
+  integer, intent(in)                :: ixImin1,ixImin2,ixImax1,ixImax2,&
+     ixOmin1,ixOmin2,ixOmax1,ixOmax2
+  double precision, intent(in)       :: x(ixImin1:ixImax1,ixImin2:ixImax2,&
+     1:ndim)
+  double precision                   :: w(ixImin1:ixImax1,ixImin2:ixImax2,&
+     nw+nwauxio)
+  double precision                   :: normconv(0:nw+nwauxio)
+
+  double precision                   :: rad_flux(ixImin1:ixImax1,&
+     ixImin2:ixImax2,1:ndim), rad_pressure(ixImin1:ixImax1,ixImin2:ixImax2),&
+      fld_lambda(ixImin1:ixImax1,ixImin2:ixImax2), fld_R(ixImin1:ixImax1,&
+     ixImin2:ixImax2)
+
+  call fld_get_radflux(w, x, ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,ixOmin2,&
+     ixOmax1,ixOmax2, rad_flux, rad_pressure)
+  call fld_get_fluxlimiter(w, x, ixImin1,ixImin2,ixImax1,ixImax2, ixOmin1,&
+     ixOmin2,ixOmax1,ixOmax2, fld_lambda, fld_R)
+
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+1)=rad_flux(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2,1)
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+2)=rad_flux(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2,2)
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+3)=rad_pressure(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2)
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+4)=fld_lambda(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2)
+  w(ixOmin1:ixOmax1,ixOmin2:ixOmax2,nw+5)=fld_R(ixOmin1:ixOmax1,&
+     ixOmin2:ixOmax2)
+
+end subroutine specialvar_output
+
+subroutine specialvarnames_output(varnames)
+! newly added variables need to be concatenated with the w_names/primnames string
+  use mod_global_parameters
+  character(len=*) :: varnames
+
+  varnames = 'F1 F2 RP lam fld_R'
+
+end subroutine specialvarnames_output
+
+!==========================================================================================
+
+end module mod_usr
+
+!==========================================================================================
---
+++ mytests/fld_test_1/mod_usr.t
@@ -0,0 +1,155 @@
+!> This is a template for a new user problem
+module mod_usr
+
+  ! Include a physics module
+  use mod_hd
+  use mod_fld
+
+  implicit none
+
+contains
+
+  !> This routine should set user methods, and activate the physics module
+  subroutine usr_init()
+    use mod_global_parameters
+    use mod_usr_methods
+
+    call set_coordinate_system("Cartesian_2D")
+
+    ! Initialize units
+    usr_set_parameters => initglobaldata_usr
+
+    ! A routine for initial conditions is always required
+    usr_init_one_grid => initial_conditions
+
+    ! Keep the radiative energy constant with internal bound
+    usr_internal_bc => constant_r_e
+
+    ! Output routines
+    usr_aux_output    => specialvar_output
+    usr_add_aux_names => specialvarnames_output
+
+    ! Active the physics module
+    call hd_activate()
+
+    print*, 'unit_time', unit_time
+    print*, 'unit_temperature', unit_temperature
+    print*, 'unit_length', unit_length
+    print*, 'unit_density', unit_density
+    !print*, 'unit_energy', unit_energy
+    print*, '================================================================'
+
+  end subroutine usr_init
+
+!==========================================================================================
+
+subroutine initglobaldata_usr
+  use mod_global_parameters
+
+  !Fix dimensionless stuff here
+  unit_length        = 1.d0                                         ! cm
+  unit_density       = 1.d0                                         ! g cm^-3
+  unit_temperature   = 1.d0                                         ! K
+
+end subroutine initglobaldata_usr
+
+!==========================================================================================
+
+  !> A routine for specifying initial conditions
+  subroutine initial_conditions(ixG^L, ix^L, w, x)
+    use mod_global_parameters
+    use mod_constants
+    use mod_hd_phys, only: hd_get_pthermal
+
+    integer, intent(in)             :: ixG^L, ix^L
+    double precision, intent(in)    :: x(ixG^S, ndim)
+    double precision, intent(inout) :: w(ixG^S, nw)
+
+    double precision :: temperature(ixG^S)
+    double precision :: int_energy(ixG^S)
+
+    integer :: i
+
+    ! Set initial values for w
+    w(ixG^S, rho_) = 1d-7
+    w(ixG^S, mom(1)) = zero
+    w(ixG^S, mom(2)) = zero
+    w(ixG^S, e_) = 1d10
+    w(ixG^S,r_e) = 1d12
+
+  end subroutine initial_conditions
+
+!==========================================================================================
+
+  ! Extra routines can be placed here
+  ! ...
+
+!==========================================================================================
+
+!> internal boundary, user defined
+  !
+  !> This subroutine can be used to artificially overwrite ALL conservative
+  !> variables in a user-selected region of the mesh, and thereby act as
+  !> an internal boundary region. It is called just before external (ghost cell)
+  !> boundary regions will be set by the BC selection. Here, you could e.g.
+  !> want to introduce an extra variable (nwextra, to be distinguished from nwaux)
+  !> which can be used to identify the internal boundary region location.
+  !> Its effect should always be local as it acts on the mesh.
+
+  subroutine constant_r_e(level,qt,ixI^L,ixO^L,w,x)
+    use mod_global_parameters
+    integer, intent(in)             :: ixI^L,ixO^L,level
+    double precision, intent(in)    :: qt
+    double precision, intent(inout) :: w(ixI^S,1:nw)
+    double precision, intent(in)    :: x(ixI^S,1:ndim)
+
+    w(ixI^S,r_e) = 1d12
+
+    print*, 'WHYSIS THIS NOT WORKING', w(50,50,e_)
+
+  end subroutine constant_r_e
+
+!==========================================================================================
+
+subroutine specialvar_output(ixI^L,ixO^L,w,x,normconv)
+! this subroutine can be used in convert, to add auxiliary variables to the
+! converted output file, for further analysis using tecplot, paraview, ....
+! these auxiliary values need to be stored in the nw+1:nw+nwauxio slots
+!
+! the array normconv can be filled in the (nw+1:nw+nwauxio) range with
+! corresponding normalization values (default value 1)
+  use mod_global_parameters
+  use mod_physics
+
+  integer, intent(in)                :: ixI^L,ixO^L
+  double precision, intent(in)       :: x(ixI^S,1:ndim)
+  double precision                   :: w(ixI^S,nw+nwauxio)
+  double precision                   :: normconv(0:nw+nwauxio)
+
+  double precision                   :: rad_flux(ixI^S,1:ndim), rad_pressure(ixI^S), fld_lambda(ixI^S), fld_R(ixI^S)
+
+  call fld_get_radflux(w, x, ixI^L, ixO^L, rad_flux, rad_pressure)
+  call fld_get_fluxlimiter(w, x, ixI^L, ixO^L, fld_lambda, fld_R)
+
+  w(ixO^S,nw+1)=rad_flux(ixO^S,1)
+  w(ixO^S,nw+2)=rad_flux(ixO^S,2)
+  w(ixO^S,nw+3)=rad_pressure(ixO^S)
+  w(ixO^S,nw+4)=fld_lambda(ixO^S)
+  w(ixO^S,nw+5)=fld_R(ixO^S)
+
+end subroutine specialvar_output
+
+subroutine specialvarnames_output(varnames)
+! newly added variables need to be concatenated with the w_names/primnames string
+  use mod_global_parameters
+  character(len=*) :: varnames
+
+  varnames = 'F1 F2 RP lam fld_R'
+
+end subroutine specialvarnames_output
+
+!==========================================================================================
+
+end module mod_usr
+
+!==========================================================================================
